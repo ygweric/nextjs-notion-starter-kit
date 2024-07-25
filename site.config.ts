a9122b1a79f1b9df3cfe67f02676ae5815c43262
yanggw <yanggw@glodon.com>
2024-07-25 13:26:34 +0800

    feat: new blog for guowei.fun

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'c7f405a90beb452f9e73b7be84ed6082',
+  rootNotionPageId: '49baec262f554681a19b992555aca028',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
