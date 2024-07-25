914f361dbe5ad2f6c4f38e7870766359c15bcefa
yanggw <yanggw@glodon.com>
2024-07-25 14:09:18 +0800

    feat: linkedin

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'ygweric',
   github: 'ygweric',
-  linkedin: 'fisch2',
+  linkedin: 'ygweric',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
